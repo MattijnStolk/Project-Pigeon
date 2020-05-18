9243ae61ee4f24448cc2beecd7f3ad93e8e9ccb4
mattijn <0986087@hr.nl>
2020-05-18 14:00:45 +0200

    first commit

---
+++ movingElementGun/docs/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+var Game = (function () {
+    function Game() {
+    }
+    return Game;
+}());
+window.addEventListener("load", function () { return new Game(); });
+//# sourceMappingURL=main.js.map
